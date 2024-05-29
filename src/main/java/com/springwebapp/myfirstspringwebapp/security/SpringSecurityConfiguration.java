790431827b025bc3c8e3e3fcdc1147f1425c57e8
Felix Chinonso Emmanuel <feelchi1star@gmail.com>
2024-05-29 02:12:52 +0100

    Hi

--- src/main/java/com/springwebapp/myfirstspringwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/springwebapp/myfirstspringwebapp/security/SpringSecurityConfiguration.java
@@ -7,17 +7,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfiguration {
     // LDAP or Database
     // IN memory
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/").permitAll().anyRequest()
+                .authenticated()); // authorize
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(header -> header.frameOptions(frameOption -> frameOption.disable()));
+        return http.build();
+    }
 
     @Bean
     InMemoryUserDetailsManager createDetailsManager() {
@@ -26,13 +40,6 @@ public class SpringSecurityConfiguration {
         UserDetails userDetails1 = createNewUser(username, password);
         UserDetails userDetails2 = createNewUser("Feel", "1234");
         return new InMemoryUserDetailsManager(userDetails1, userDetails2);
-    }
-
-    private UserDetails createNewUser(String username, String password) {
-        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-        UserDetails userDetails = User.builder().username(username).passwordEncoder(passwordEncoder).password(password)
-                .roles("USER", "ADMIN").build();
-        return userDetails;
     }
 
     @Bean
@@ -45,13 +52,16 @@ public class SpringSecurityConfiguration {
     // CSRF disable
     // Frames
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()); // authorize
-        http.formLogin(Customizer.withDefaults());
-        http.csrf(csrf -> csrf.disable());
-        http.headers(header -> header.frameOptions(frameOption -> frameOption.disable()));
-        return http.build();
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        UserDetails userDetails = User.builder().username(username).passwordEncoder(passwordEncoder).password(password)
+                .roles("USER", "ADMIN").build();
+        return userDetails;
     }
+
+    // @Bean
+    // public WebSecurityCustomizer webSecurityCustomizer() {
+    // return (web) -> web.ignoring().requestMatchers("/");
+    // }
 
 }
